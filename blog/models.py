a62f9c672215cc2ce813014420af8d47b19f1d54
Rona <ronalynasio@gmail.com>
2026-09-20 23:34:06 +0800

    add app+fields

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+created = models.DateTimeField(auto_now_add=True)
+
+
+class Meta:
+    ordering = ['-published']
+    indexes = [
+        models.Index(fields=['-published'])
+    
+    ]
+
+    def __str__(self):
+        return self.title
+
+
+    
